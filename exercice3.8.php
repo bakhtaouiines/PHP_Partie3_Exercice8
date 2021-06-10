eb20fc0d21a0c0435a9677e4f712c59c56dcf984
Ines Bakhtaoui <inesbkht@gmail.com>
2021-06-10 10:55:50 +0200

    exercice 3.8 fait

---
+++ exercice3.8.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 3.8</title>
+</head>
+<body>
+    <h1>Exercice 8 Partie 3 : Les boucles PHP</h1>
+    <p>
+    <?php
+    for($firstNumber = 200; $firstNumber >=0; $firstNumber-=12)
+    {
+        echo ("Enfin!!!! ");
+    }
+    ?>
+    </p>
+</body>
+</html>
